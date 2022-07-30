7289254c0dc3e841d8abcec06d4e0410739eb760
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-07-30 18:24:00 +0600

    first commit

---
+++ fundametal.js
@@ -0,0 +1,20 @@
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+
+// // console.log(typeof 25);
+// // console.log(typeof "Sazzad");
+
+// javascriptIsFun = "Yes";
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(typeof year);
+
+// year = 1996;
+// console.log(typeof year);
+
+let age = 30;
+age = 31;
+console.log(age);
+//let diye jei variable likhbo shetake abar reassing kora jay.mane ami chaile proyojon onujai (let variable) er value ke change korte parbo.
