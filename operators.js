ccaa029d7f732f9374f3dcdd086edd56b4742210
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-08-04 18:35:09 +0600

    asterisk sign

--- operators.js
+++ operators.js
@@ -6,3 +6,11 @@
  * 3) logical operators.
  * 4) assignment operators and many more.
  */
+// (*) ati hocche asterisk sign.
+const nowYear = 2022;
+const sazzadAge = nowYear - 1996;
+const fahimAge = nowYear - 1989;
+console.log(sazzadAge, fahimAge);
+
+console.log(sazzadAge * 2, fahimAge / 2, 2 ** 3);
+// ai khane (2 ** 3) er mane holo (2*2*2) 3 ta 2.
