c3aabbc9d362aa9855140a7c32003f421466335d
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-08-29 22:32:36 +0600

    change some code

--- practice.js
+++ practice.js
@@ -27,3 +27,4 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(bills, tips, total);
+/////////////////////////////////////////
