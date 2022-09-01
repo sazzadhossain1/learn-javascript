14d3ad1369730cedee8b2680c5fa972211e9146d
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-09-02 02:19:24 +0600

    add Event Listener

--- projectOne.js
+++ projectOne.js
@@ -1,8 +1,18 @@
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "Correct Number";
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
-console.log(document.querySelector(".guess").value);
-document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "Correct Number";
+
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+
+// console.log(document.querySelector(".guess").value);
+// document.querySelector(".guess").value = 23;
 
 // kono akta html er taga ke jodi amra dhorte chai tahole class, id er maddhome dhorte parbo.......amra querySelector er vitore class othoba id use korbo.....class use korar aage (.) use korbo and id use korar aage (#) use korbo.....pore textContent likhe (=) er pore ja likhbo sheta replace hoye jabe......kintu amra jodi kono input filled ke dhorte chai tahole aki avbe kaj korbo shodhu textContent er poriborte (.value) use korbo.....
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    console.log((document.querySelector(".message").textContent = "No Number"));
+  }
+});
