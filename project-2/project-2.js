9de4a539304c40cbb244af34284543823920f8d1
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-09-05 20:15:14 +0600

    refactorying some code

--- project-2/project-2.js
+++ project-2/project-2.js
@@ -2,8 +2,23 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+const body = document.querySelector('body');
+
+const openModal = function () {
+  console.log('Click Button');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i = i + 1) {
-  console.log(btnsOpenModal[i]);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
