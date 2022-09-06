669b935a7cbc549b22aea5556fc55505eca9f356
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-09-07 01:27:41 +0600

    practice

--- project-3/project-3.js
+++ project-3/project-3.js
@@ -1,7 +1,10 @@
-// Selecting Element //
+// // Selecting Element //
 
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -12,6 +15,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+let currentScore = 0;
+
 // Create Roll Dice Function //
 btnRoll.addEventListener("click", function () {
   // generating a random dice roll //
@@ -20,4 +25,45 @@ btnRoll.addEventListener("click", function () {
   // display the dice photo with dynamicaly//
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${dice}.png`;
+
+  //-------------------------------------
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  }
 });
+
+//----------------------------------------------------
+
+// const sorce0Ele = document.querySelector("#score--0");
+// const sorce1Ele = document.getElementById("score--1");
+// const current0El = document.getElementById("current--0");
+// const current1El = document.getElementById("current--1");
+
+// const diceEl = document.querySelector(".dice");
+// const btnNew = document.querySelector(".btn--new");
+// const btnroll = document.querySelector(".btn--roll");
+// const btnHold = document.querySelector(".btn--hold");
+
+// // selecting condition //
+
+// sorce0Ele.textContent = 0;
+// sorce1Ele.textContent = 0;
+// diceEl.classList.add("hidden");
+
+// let currentScore = 0;
+
+// btnroll.addEventListener("click", function () {
+//   const dice = Math.trunc(Math.random() * 6) + 1;
+//   diceEl.classList.remove("hidden");
+//   diceEl.src = `dice-${dice}.png`;
+
+//   //----------------------------//
+
+//   if (dice !== 1) {
+//     currentScore += dice;
+
+//     current0El.textContent = currentScore;
+//   }
+// });
