24be0afe10280a4b06e2634c2afc9f850b3b5785
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-09-06 16:54:12 +0600

    get players score

--- project-3/project-3.js
+++ project-3/project-3.js
@@ -1,1 +1,10 @@
+// Selecting Element //
 
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const dice = document.querySelector(".dice");
+
+// Selecting Condition //
+score0El.textContent = 0;
+score1El.textContent = 0;
+dice.classList.add("hidden");
