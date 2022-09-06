0c1f20e40870611d9b3afca36b1a7b87e6eef35f
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-09-06 18:55:25 +0600

    show the dice photo with dynamicaly

--- project-3/project-3.js
+++ project-3/project-3.js
@@ -2,12 +2,22 @@
 
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
-const dice = document.querySelector(".dice");
-const btnNew = document.querySelector("btn--new");
-const btnRoll = document.querySelector("btn--roll");
-const btnHold = document.querySelector("btn--hold");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 // Selecting Condition //
 score0El.textContent = 0;
 score1El.textContent = 0;
-dice.classList.add("hidden");
+diceEl.classList.add("hidden");
+
+// Create Roll Dice Function //
+btnRoll.addEventListener("click", function () {
+  // generating a random dice roll //
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // display the dice photo with dynamicaly//
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+});
