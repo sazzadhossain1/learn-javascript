6afb74bfb4a14d5879084fbba17a658429bd7929
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-09-08 17:06:45 +0600

    daynamicaly access player

--- project-3/project-3.js
+++ project-3/project-3.js
@@ -15,7 +15,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Create Roll Dice Function //
 btnRoll.addEventListener("click", function () {
@@ -30,7 +32,9 @@ btnRoll.addEventListener("click", function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // current0El.textContent = currentScore;
   }
 });
 
