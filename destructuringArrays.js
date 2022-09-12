8d35e033fd0e88c27839c2305949b090e357b62c
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-09-12 18:34:54 +0600

    destructuring array

---
+++ destructuringArrays.js
@@ -0,0 +1,15 @@
+const restaurant = {
+  name: "classico Italiano",
+  location: "Via angelo Tavanti 23, Firenze,Italy",
+  categories: ["Italian", "pizzeria", "Vegetarian", "organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+};
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
--- practice.js
+++ practice.js
@@ -1,30 +1,42 @@
-const calcAverage = (a, b, c) => {
-  return (a + b + c) / 3;
-};
-// const scoreDolphin = calcAverage(44, 23, 71);
-// const scoreKoalas = calcAverage(65, 54, 49);
-// console.log(scoreDolphin, scoreKoalas);
+// const calcAverage = (a, b, c) => {
+//   return (a + b + c) / 3;
+// };
+// // const scoreDolphin = calcAverage(44, 23, 71);
+// // const scoreKoalas = calcAverage(65, 54, 49);
+// // console.log(scoreDolphin, scoreKoalas);
 
-const checkWinner = function (avgDolphin, avgKoalas) {
-  if (avgDolphin >= 2 * avgKoalas) {
-    console.log(`Dolphine is winner ${avgDolphin} vs. ${avgKoalas}`);
-  } else if (avgKoalas >= 2 * avgDolphin) {
-    console.log(`Koalas is Winner ${avgKoalas} vs. ${avgDolphin}`);
-  } else {
-    console.log("The match is draw ");
-  }
-};
-const scoreDolphin = calcAverage(85, 54, 41);
-const scoreKoalas = calcAverage(23, 34, 27);
+// const checkWinner = function (avgDolphin, avgKoalas) {
+//   if (avgDolphin >= 2 * avgKoalas) {
+//     console.log(`Dolphine is winner ${avgDolphin} vs. ${avgKoalas}`);
+//   } else if (avgKoalas >= 2 * avgDolphin) {
+//     console.log(`Koalas is Winner ${avgKoalas} vs. ${avgDolphin}`);
+//   } else {
+//     console.log("The match is draw ");
+//   }
+// };
+// const scoreDolphin = calcAverage(85, 54, 41);
+// const scoreKoalas = calcAverage(23, 34, 27);
 
-// checkWinner(scoreDolphin, scoreKoalas);
+// // checkWinner(scoreDolphin, scoreKoalas);
 
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-console.log(bills, tips, total);
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(bills, tips, total);
 /////////////////////////////////////////
+
+const arr = [1, 2, 3];
+arr.push(4);
+console.log(arr);
+const hasZero = arr.indexOf(1) > -1;
+console.log(hasZero);
+//////////////////////////////
+
+let x = 44;
+let y = 55;
+x = "sazzad";
+console.log(x);
