6dbecd6ad80521a1bfc3e38e5e8a7aa6665bbf9f
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-10-07 00:25:30 +0600

    function return function is done

--- functionReturnFunction.js
+++ functionReturnFunction.js
@@ -9,3 +9,27 @@ const greet = function (greeting) {
 // greeterHey("Solim");
 
 greet("Hello")("Mr. Sazzad");
+
+const greet1 = (greeting) => {
+  return (name) => {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+greet1("Hello")("Mr. Yeasin");
+
+const name1 = ["Sazzad", "Hossain", "Tomal"];
+
+const fun = function (num) {
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] === "Sazzad") {
+      console.log(num[i]);
+    }
+    return function (text) {
+      const comment = num[i] + " " + text;
+      console.log(comment);
+    };
+  }
+};
+
+fun(name1)("How are you ?");
