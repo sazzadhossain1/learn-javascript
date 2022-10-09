9f29c9f83b7781ebfd22658f0a9fe09ea041393f
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-10-09 16:49:22 +0600

    code challange done

--- codeChallange.js
+++ codeChallange.js
@@ -554,11 +554,41 @@ console.log(minutes);
 // Coding Challange --- 1 //
 
 const poll = {
-  question: "What is your favourite programming Language?",
-  options: ["0: Javasctipt", "1: Python", "2: Rust", "3: C++"],
+  question: "What is your favourite programming language?",
+  options: ["0: Javascript", "1: Python", "2: Rust", "3: C++"],
 
   answers: new Array(4).fill(0),
+
   registerNewAnswer() {
-    const answer = prompt();
+    // Get the answer //
+    let answer = Number(
+      prompt(
+        `${this.question}\n ${this.options.join("\n")}\n Write option number`
+      )
+    );
+    console.log(answer);
+
+    // Register Answer //
+    typeof answer === "number" &&
+      answer < this.answers.length &&
+      this.answers[answer++];
+    this.displayResults();
+    this.displayResults("string");
+  },
+  displayResults(type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      console.log(`poll results are ${this.answers.join(", ")}`);
+    }
   },
 };
+
+// poll.registerNewAnswer();
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
