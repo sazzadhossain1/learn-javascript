d564ed1e73629f867355ef1aa9f20dab3e15484d
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-10-10 18:37:00 +0600

    closure coding challange 2 is done

--- codeChallange.js
+++ codeChallange.js
@@ -553,45 +553,45 @@ console.log(minutes);
 // A Closer Look at Functions //
 // Coding Challange --- 1 //
 
-const poll = {
-  question: "What is your favourite programming language?",
-  options: ["0: Javascript", "1: Python", "2: Rust", "3: C++"],
+// const poll = {
+//   question: "What is your favourite programming language?",
+//   options: ["0: Javascript", "1: Python", "2: Rust", "3: C++"],
 
-  answers: new Array(4).fill(0),
+//   answers: new Array(4).fill(0),
 
-  registerNewAnswer() {
-    // Get the answer //
-    let answer = Number(
-      prompt(
-        `${this.question}\n ${this.options.join("\n")}\n Write option number`
-      )
-    );
-    console.log(answer);
+//   registerNewAnswer() {
+//     // Get the answer //
+//     let answer = Number(
+//       prompt(
+//         `${this.question}\n ${this.options.join("\n")}\n Write option number`
+//       )
+//     );
+//     console.log(answer);
 
-    // Register Answer //
-    typeof answer === "number" &&
-      answer < this.answers.length &&
-      this.answers[answer++];
-    this.displayResults();
-    this.displayResults("string");
-  },
-  displayResults(type = "array") {
-    if (type === "array") {
-      console.log(this.answers);
-    } else if (type === "string") {
-      console.log(`poll results are ${this.answers.join(", ")}`);
-    }
-  },
-};
+//     // Register Answer //
+//     typeof answer === "number" &&
+//       answer < this.answers.length &&
+//       this.answers[answer++];
+//     this.displayResults();
+//     this.displayResults("string");
+//   },
+//   displayResults(type = "array") {
+//     if (type === "array") {
+//       console.log(this.answers);
+//     } else if (type === "string") {
+//       console.log(`poll results are ${this.answers.join(", ")}`);
+//     }
+//   },
+// };
 
-// poll.registerNewAnswer();
-document
-  .querySelector(".poll")
-  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+// // poll.registerNewAnswer();
+// document
+//   .querySelector(".poll")
+//   .addEventListener("click", poll.registerNewAnswer.bind(poll));
 
-poll.displayResults.call({ answers: [5, 2, 3] }, "string");
-poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
-poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+// poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+// poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
+// poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 
 //---------------------------------------------------------//
 
@@ -601,4 +601,8 @@ poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 (function () {
   const header = document.querySelector("h1");
   header.style.color = "red";
+
+  document.querySelector(".poll").addEventListener("click", function () {
+    header.style.color = "blue";
+  });
 })();
