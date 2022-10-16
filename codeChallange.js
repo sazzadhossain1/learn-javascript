57f4fea8dddfe1e48ccc9260d28c4c6b26e16405
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-10-16 17:03:27 +0600

    code challange done

--- codeChallange.js
+++ codeChallange.js
@@ -638,7 +638,15 @@ console.log("********************************");
 // Coding Challange -- 2
 const calcAverageHumanAge = function (ages) {
   const humanAge = ages.map((age) => (age <= 2 ? age * 2 : 16 + age * 4));
-
+  const adults = humanAge.filter((age) => age >= 18);
+  console.log(adults);
   console.log(humanAge);
+
+  const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+  return average;
 };
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1]);
+
+console.log(avg1, avg2);
