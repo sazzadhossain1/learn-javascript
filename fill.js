53ea6a8c9244823279ec191bbbefa1183ddedde9
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-10-21 00:52:27 +0600

    fill method start

---
+++ fill.js
@@ -0,0 +1,2 @@
+console.log([1, 2, 3, 4, 5, 6, 7]);
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
--- sortingArray.js
+++ sortingArray.js
@@ -1,12 +1,12 @@
 //String
-const owners = ["Jonas", "Zach", "Adam", "Martha"];
-console.log(owners.sort());
+// const owners = ["Jonas", "Zach", "Adam", "Martha"];
+// console.log(owners.sort());
 
-// Numbers
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// // Numbers
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-movements.sort((a, b) => b - a);
-console.log(movements);
+// movements.sort((a, b) => b - a);
+// console.log(movements);
 //-----------------------------
 
 // Sorting by another way
@@ -20,52 +20,52 @@ console.log(movements);
 // console.log(movements);
 //----------------------------------------------
 // My won practice //
-// const letter = [
-//   "a",
-//   "d",
-//   "c",
-//   "b",
-//   "g",
-//   "f",
-//   "h",
-//   "j",
-//   "i",
-//   "e",
-//   "k",
-//   "l",
-//   "m",
-//   "n",
-//   "o",
-//   "p",
-//   "r",
-//   "y",
-//   "x",
-//   "q",
-//   "s",
-//   "w",
-//   "u",
-//   "v",
-//   "z",
-//   "t",
-//   "a",
-//   "l",
-//   "s",
-//   "m",
-//   "o",
-//   "b",
-//   "y",
-// ];
+const letter = [
+  "a",
+  "d",
+  "c",
+  "b",
+  "g",
+  "f",
+  "h",
+  "j",
+  "i",
+  "e",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "r",
+  "y",
+  "x",
+  "q",
+  "s",
+  "w",
+  "u",
+  "v",
+  "z",
+  "t",
+  "a",
+  "l",
+  "s",
+  "m",
+  "o",
+  "b",
+  "y",
+];
 
-// // remove duplicate number using filter function
-// const emptyArray = [];
-// function removeDuplicagtes(letterArray) {
-//   return emptyArray.push(
-//     letterArray.filter((le, i) => letterArray.indexOf(le) === i)
-//   );
-// }
-// removeDuplicagtes(letter);
-// console.log(emptyArray);
+// remove duplicate number using filter function
+const emptyArray = [];
+function removeDuplicagtes(letterArray) {
+  return emptyArray.push(
+    letterArray.filter((le, i) => letterArray.indexOf(le) === i)
+  );
+}
+removeDuplicagtes(letter);
+console.log(emptyArray);
 
-// const flatMethod = emptyArray.flat();
-// const sortElement = flatMethod.sort().join(" ").toUpperCase();
-// console.log(sortElement);
+const flatMethod = emptyArray.flat();
+const sortElement = flatMethod.sort().join(" ").toUpperCase();
+console.log(sortElement);
