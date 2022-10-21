b8f0492494b73245186946eb0d595f0885d1f287
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-10-22 01:37:07 +0600

    coding challange start

--- codeChallange.js
+++ codeChallange.js
@@ -657,3 +657,17 @@ const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
 console.log(avg1, avg2);
 //-------------------------------------//
+
+//----------------/---------------------------//
+
+console.log("=======================================");
+
+// Working With Arrays //
+// Coding Challange -- 4
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+  { weight: 8, curFood: 200, owners: ["Matilda"] },
+  { weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+  { weight: 32, curFood: 340, owners: ["Michael"] },
+];
