08b7624fbc3ae83f4dd4878a110d9bf674b9ccf9
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-10-23 03:14:20 +0600

    date and time

---
+++ datesAndTime.js
@@ -0,0 +1,62 @@
+const account1 = {
+  owner: "Sazzad SHossain",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+// const accounts = [account1, account2, account3, account4];
+
+// const now = new Date();
+// console.log(now);
+// console.log(new Date("2022-10-22T20:56:36.869Z"));
+// console.log(new Date("December 24,2015"));
+// console.log(new Date("December 24,2015"));
+// // console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 10, 19, 15, 23, 5));
+// console.log(new Date(2037, 10, 31));
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142235380000));
+
+console.log(Date.now());
+
+future.setFullYear(2090);
+console.log(future);
