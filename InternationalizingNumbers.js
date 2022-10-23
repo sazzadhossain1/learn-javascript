0fc090c8c77923a5ec1c4c0810057b38b563a099
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-10-24 00:52:31 +0600

    setTimeOut

--- InternationalizingNumbers.js
+++ InternationalizingNumbers.js
@@ -7,10 +7,22 @@ const options = {
   //   useGrouping: false,
 };
 
-console.log("US:      ", new Intl.NumberFormat("en-US").format(num));
-console.log("Germany:", new Intl.NumberFormat("de-DE").format(num));
-console.log("Syria:", new Intl.NumberFormat("ar-SY").format(num));
-// console.log(
+// console.log("US:      ", new Intl.NumberFormat("en-US").format(num));
+// console.log("Germany:", new Intl.NumberFormat("de-DE").format(num));
+// console.log("Syria:", new Intl.NumberFormat("ar-SY").format(num));
+// // console.log(
 //   navigator.languages,
 //   new Intl.NumberFormat(navigator.language).format(num)
 // );
+
+//////////////////////////////////////////
+////////////////////////////
+///////////////////
+
+setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}`),
+  3000,
+  "olives",
+  "spinach"
+);
+console.log("Waiting...");
