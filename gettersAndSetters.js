3d3c387a7b6abee9616ce07b267d6c1a21d1efed
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-10-30 01:53:49 +0600

    getters and setters done

--- gettersAndSetters.js
+++ gettersAndSetters.js
@@ -1,5 +1,5 @@
 class Computer {
-  constructor(name, model) {
+  constructor(name, model, price) {
     this.name = name;
     this.model = model;
   }
@@ -9,8 +9,32 @@ class Computer {
       `this computer name is ${this.name} and the model is ${this.model}`
     );
   }
+
+  get computerName() {
+    return this.name;
+  }
+
+  get computerModel() {
+    return this.model;
+  }
+
+  set computerName(name) {
+    this.name = name;
+  }
+
+  set computerModel(model) {
+    this.model = model;
+  }
 }
 
 const dell = new Computer("Dell", "007");
 console.log(dell);
 dell.aboutComputer();
+
+dell.computerName = "HP";
+console.log(dell.computerName);
+console.log(dell.computerModel);
+console.log(dell);
+
+dell.computerModel = "1o1";
+console.log(dell);
