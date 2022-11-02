143ea35f7c4bb6ae3063a92175cfa8c17a92de46
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-11-03 01:16:25 +0600

    copy some html and css code

--- ajax.js
+++ ajax.js
@@ -1,41 +1,58 @@
-const getButton = document.querySelector("#get-button");
-const sendButton = document.querySelector("#send-button");
+// Learn With sumit vai //
+//.....................//
 
-const sendRequest = function (method, url, data) {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.responseType = "json";
-    xhr.send(data);
+// const getButton = document.querySelector("#get-button");
+// const sendButton = document.querySelector("#send-button");
 
-    xhr.onload = function () {
-      resolve(xhr.response);
-    };
-  });
-  return promise;
-};
+// const sendRequest = function (method, url, data) {
+//   const promise = new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open(method, url);
+//     xhr.responseType = "json";
+//     xhr.send(data);
 
-const getDat = function () {
-  sendRequest(
-    "GET",
-    "https://jsonplaceholder.typicode.com/todos/1",
-    JSON.stringify({
-      title: "foo",
-      body: "bar",
-      userId: 1,
-    })
-  ).then((responseData) => {
-    console.log(responseData);
-  });
-};
+//     xhr.onload = function () {
+//       resolve(xhr.response);
+//     };
+//   });
+//   return promise;
+// };
 
-const sendData = function () {
-  sendRequest("POST", "https://jsonplaceholder.typicode.com/posts").then(
-    (responseData) => {
-      console.log(responseData);
-    }
-  );
-};
+// const getDat = function () {
+//   sendRequest(
+//     "GET",
+//     "https://jsonplaceholder.typicode.com/todos/1",
+//     JSON.stringify({
+//       title: "foo",
+//       body: "bar",
+//       userId: 1,
+//     })
+//   ).then((responseData) => {
+//     console.log(responseData);
+//   });
+// };
 
-getButton.addEventListener("click", getDat);
-sendButton.addEventListener("click", sendData);
+// const sendData = function () {
+//   sendRequest("POST", "https://jsonplaceholder.typicode.com/posts").then(
+//     (responseData) => {
+//       console.log(responseData);
+//     }
+//   );
+// };
+
+// getButton.addEventListener("click", getDat);
+// sendButton.addEventListener("click", sendData);
+
+////////////////////////////////////////////////////////////
+//-------------------------------------------------------//
+//////////////////////////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open("GET", "https://jsonplaceholder.typicode.com/posts");
+request.send();
+
+request.addEventListener("load", function () {
+  const [data] = JSON.parse(this.responseText);
+
+  console.log(data);
+});
