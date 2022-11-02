5a9694dff539bd28f224d7252233656a446fe6e4
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-11-02 18:04:27 +0600

    get api and send api

--- ajax.js
+++ ajax.js
@@ -1,1 +1,41 @@
+const getButton = document.querySelector("#get-button");
+const sendButton = document.querySelector("#send-button");
 
+const sendRequest = function (method, url, data) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = "json";
+    xhr.send(data);
+
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+  });
+  return promise;
+};
+
+const getDat = function () {
+  sendRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/todos/1",
+    JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    })
+  ).then((responseData) => {
+    console.log(responseData);
+  });
+};
+
+const sendData = function () {
+  sendRequest("POST", "https://jsonplaceholder.typicode.com/posts").then(
+    (responseData) => {
+      console.log(responseData);
+    }
+  );
+};
+
+getButton.addEventListener("click", getDat);
+sendButton.addEventListener("click", sendData);
