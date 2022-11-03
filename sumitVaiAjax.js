801c4fc9bb56dc711863923b30bb7599541c7a90
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-11-03 15:36:01 +0600

    practice XMLHttp Request with deffrent ways

--- sumitVaiAjax.js
+++ sumitVaiAjax.js
@@ -1,15 +1,97 @@
-const loadData = function () {
-  const xhr = new XMLHttpRequest();
+// const loadData = function () {
+//   const xhr = new XMLHttpRequest();
 
-  // What to do when response arrives
-  xhr.onload = function () {
-    const container = document.getElementById("demo");
-    demo.innerHTML = xhr.responseText;
-  };
+//   // What to do when response arrives
+//   xhr.onload = function () {
+//     const container = document.getElementById("demo");
+//     demo.innerHTML = xhr.responseText;
+//   };
 
-  // Preparerequest - methods: GET, POST, PARCH, DELETE, OPTIONS
-  xhr.open("GET", "./sumitVaiAjax.text");
+//   // Preparerequest - methods: GET, POST, PARCH, DELETE, OPTIONS
+//   xhr.open("GET", "./sumitVaiAjax.text");
 
-  // Send request
-  xhr.send();
-};
+//   // Send request
+//   xhr.send();
+// };
+
+///////////////////////////////////////////////
+
+// const loadData = function () {
+//   const xhr = new XMLHttpRequest();
+
+//   xhr.onload = function () {
+//     const container = document.getElementById("demo");
+//     demo.innerHTML = xhr.responseText;
+//   };
+//   xhr.open("GET", "./sumitVaiAjax.text");
+//   xhr.send();
+// };
+
+/////////////////////////////////////////////////////
+
+// arrow function
+//...............
+
+// const loadData = () => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.onload = function () {
+//     const container = document.querySelector("#demo");
+//     demo.innerHTML = xhr.responseText;
+//   };
+//   xhr.open("GET", "./sumitVaiAjax.text");
+//   xhr.send();
+// };
+////////////////////////////////////////////////////////////
+
+// const loadData = function () {
+//   const xhr = new XMLHttpRequest();
+
+//   xhr.onload = function () {
+//     const container = document.querySelector("#demo");
+//     demo.innerHTML = xhr.responseText;
+//   };
+
+//   xhr.open("GET", "./sumitVaiAjax.text");
+//   xhr.send();
+// };
+
+//////////////////////////////////////////////////////
+
+// const loadData = () => {
+//   const xhr = new XMLHttpRequest();
+
+//   xhr.onload = () => {
+//     const container = document.querySelector("#demo");
+//     demo.innerHTML = xhr.responseText;
+//   };
+//   xhr.open("GET", "./sumitVaiAjax.text");
+//   xhr.send();
+// };
+
+//////////////////////////////////////////////////////
+
+// const loadData = function () {
+//   const xhr = new XMLHttpRequest();
+
+//   xhr.onload = () => {
+//     const container = document.querySelector("#demo");
+//     demo.innerHTML = xhr.responseText;
+//   };
+
+//   xhr.open("GET", "./sumitVaiAjax.text");
+//   xhr.send();
+// };
+
+//////////////////////////////////////////
+
+const getButton = document
+  .querySelector("#btn")
+  .addEventListener("click", function () {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+      const container = document.querySelector("#demo");
+      demo.innerHTML = xhr.responseText;
+    };
+    xhr.open("GET", "./sumitVaiAjax.text");
+    xhr.send();
+  });
