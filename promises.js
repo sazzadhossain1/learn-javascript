a16b8958052b8efe87a032a3b145fce74d627a8c
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-11-04 18:37:27 +0600

    practice promise method

--- promises.js
+++ promises.js
@@ -1,4 +1,37 @@
-const status = false;
+// const getCountryData = function (county) {
+//   fetch(`https://restcountries.com/v3.1/name/${county}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+
+//     });
+// };
+
+// const getCountryData = function (county) {
+//   fetch(`https://restcountries.com/v3.1/name/${county}`)
+//     .then((response) => {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       //   const neighbour = data.borders[0];
+//       //   console.log(neighbour);
+//     });
+// };
+
+// getCountryData("portugal");
+
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+console.log("------------ Learn with sumit vai --------------");
+
+const status = true;
 
 console.log("task 1");
 
@@ -10,9 +43,10 @@ const promise = new Promise(function (resolve, reject) {
     } else {
       reject("failed");
     }
-  }, 2000);
+  }, 1000);
 });
 
+// call promise function
 promise
   .then(function (value) {
     console.log(value);
@@ -21,3 +55,92 @@ promise
     console.log(err);
   });
 console.log("task 3");
+
+///////////////////////////////////////////////
+//////////////////////////////////////////////
+/////////////////////////////////////////////
+
+console.log("---------- Lear with Stack Learner ----------");
+
+// make promise
+//.............
+
+// let pro = true;
+
+// let p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (pro) {
+//       resolve("one");
+//     } else {
+//       reject("Error");
+//     }
+//   }, 1000);
+// });
+
+// p1.then((v) => {
+//   console.log(v);
+// });
+
+//......................................
+
+// const promi = false;
+// const p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (promi === true) {
+//       resolve("Problem is solve");
+//     } else {
+//       reject("Something is wong with the problem");
+//     }
+//   }, 1000);
+// });
+
+// p1.then((v) => {
+//   console.log(v);
+// }).catch((v) => {
+//   console.log(v);
+// });
+
+//......................................
+
+// const p1 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 3000, "One");
+// });
+
+// const p2 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 2000, "Tow");
+// });
+
+// p1.then(function (v) {
+//   console.log(v);
+// });
+
+// p2.then((v) => {
+//   console.log(v);
+// });
+
+//...........................................
+
+const exm = "Passed";
+
+// promise defination
+
+const promiseMethod = new Promise((resolve, reject) => {
+  setTimeout(function () {
+    if (exm === "Passed") {
+      resolve("Take money and go the market and bying the your dream Phone");
+    } else {
+      reject(
+        "You can't passed the exm that's why i don't give you any money for the PHONE....Go get back to your stady table"
+      );
+    }
+  }, 1000);
+});
+
+// promise call
+promiseMethod
+  .then(function (v) {
+    console.log(v);
+  })
+  .catch((v) => {
+    console.log(v);
+  });
