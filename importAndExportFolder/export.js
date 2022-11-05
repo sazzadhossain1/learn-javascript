ffe76f7e3088b887d91e992cb88e7b37604084d5
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-11-06 02:38:06 +0600

    ES6 module

---
+++ importAndExportFolder/export.js
@@ -0,0 +1,37 @@
+const shippingCost = 10;
+const cart = [];
+
+export const addToCard = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+export const totalPrice = 237;
+export const totalQuantity = 23;
+////////////////////////////////////////////////////
+///////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+// learn with sumit vai
+//.........................
+
+// export const pi = 30.1;
+// export const a = 5.55;
+
+// export const pi = 30.1;
+// const a = 5.55;
+
+// // Export with Defatult
+// export default a;
+
+// // Function Export
+// export function myFunction() {
+//   let sum = 1 + 10;
+//   return sum;
+// }
+// // Function export with default
+// function myFunction() {
+//   let sum = 1 + 10;
+//   return sum;
+// }
+// akhon ai function de default korte parbo na karon default akbari kora jay default multitime kora jay na......//
---
+++ importAndExportFolder/import.js
@@ -0,0 +1,42 @@
+// import { addToCard, totalPrice, totalQuantity } from "./export.js";
+
+// change import variable name
+// import {
+//   addToCard,
+//   totalPrice as price,
+//   totalQuantity as quantity,
+// } from "./export.js";
+
+// import all data from export
+import * as exportAll from "./export.js";
+//...........................................
+
+// console.log("Importing module");
+// addToCard("bread", 5);
+
+// console.log(totalPrice, totalQuantity);
+// console.log(price, quantity);
+
+//...........................................
+
+exportAll.addToCard("bread", 5);
+console.log(exportAll.totalPrice);
+///////////////////////////////////////////////////
+/////////////////////////////////////////////////
+///////////////////////////////////////////////
+
+// learn with sumit vai
+//.........................
+
+// import { pi, a } from "./export.js";
+// console.log(pi, a);
+
+// Import with another way
+// import fromExport, { pi } from "./export.js";
+// // console.log(fromExport);
+// // console.log(pi);
+
+// // Function Import
+// import { myFunction } from "./export.js";
+// // Call myFunction
+// console.log(myFunction());
